07ff1bed65df2067d5b34aedd0142bc41f40040f
christianloring <christianrloring@gmail.com>
2023-04-10 14:31:28 -0400

    Working token retreival and displays error on bad login

--- front_end/src/components/HomePage.tsx
+++ front_end/src/components/HomePage.tsx
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export function HomeLanding()
-{
-    return (
-        <main>
-            <h3>WELCOME</h3>
-            <div>
-            <label><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required/>
-            <br/>
-            <label><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required/>
-            <button>LOG IN</button>
-            </div>
-        </main>
-    );
+export function HomeLanding() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLoginClick = () => {
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        user_password: password
+      })
+    })
+      .then(response => {
+        if(!response.ok) {
+        setErrorMessage('Invalid username or password');}
+        else {return response.json();}
+    })
+      .then(data => {
+        const token = data.token;
+        setToken(token);
+        // Extract the token from the response data
+        // Use the token to authenticate requests to your Rails API
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  console.log(token);
+
+  return (
+    <main>
+      <h3>WELCOME</h3>
+      <div>
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required value={username} onChange={handleUsernameChange} />
+        <br />
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required value={password} onChange={handlePasswordChange} />
+        <button onClick={handleLoginClick}>LOG IN</button>
+      </div>
+      <div>
+        {errorMessage}
+      </div>
+    </main>
+  );
 }
--- front_end/src/models/users.tsx
+++ front_end/src/models/users.tsx
@@ -1,1 +1,13 @@
-import React from 'react';
+export class User {
+    id: number;
+    token: string;
+    name: string;
+    userType: string;
+    
+    constructor(id: number, token: string, name: string){
+        this.id = id;
+        this.token = token;
+        this.name = name;
+        this.userType = "user";
+    }
+}
