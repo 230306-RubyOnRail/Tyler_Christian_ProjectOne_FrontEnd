5483a3a0b30297d624ba2e6119426c226d9c2972
TDK <name20411@gmail.com>
2023-04-13 12:46:30 -0400

    April 13, 12_46pm

--- front_end/src/components/CreateEmployee.tsx
+++ front_end/src/components/CreateEmployee.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import {useState} from 'react'
 export function CreateEmployee() {
+    const [eUsrName, set_eUsrName] = useState('');
+    const [eUsrPassword, set_eUsrPassword] = useState('');
+    const [eUsrType, set_eUserType] = useState('');
+
+    const handleAccountMaker = () =>{
+        console.log("CREATE ACCOUNT!!!1");
+        //fetch()
+    }
     return(<div>
         <h1>CREATE EMPLOYEE</h1>
+        <></>
     </div>);
 }
